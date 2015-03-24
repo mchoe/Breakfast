6d2181a13a1be2ac8f257b21d58a2f3aff3fa486
mchoe <michael@straussmade.com>
2015-03-24 17:45:18 -0500

    Added NSObject extensions

---
+++ Breakfast/NSObject+Extensions.swift
@@ -0,0 +1,72 @@
+//
+//  NSObject+Extensions.swift
+//  Breakfast
+//  Start you next Swift project off right with Breakfast
+//
+//
+//  Copyright (c) 2015 Michael Choe
+//  http://www.straussmade.com/
+//  http://www.twitter.com/_mchoe
+//  http://www.github.com/mchoe
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+
+extension NSObject {
+    
+    //
+    // Retrieves an array of property names found on the current object
+    // using Objective-C runtime functions for introspection:
+    // https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtPropertyIntrospection.html
+    //
+    func propertyNames() -> Array<String> {
+        var results: Array<String> = [];
+        
+        // retrieve the properties via the class_copyPropertyList function
+        var count: UInt32 = 0;
+        var myClass: AnyClass = self.classForCoder;
+        var properties = class_copyPropertyList(myClass, &count);
+        
+        // iterate each objc_property_t struct
+        for var i: UInt32 = 0; i < count; i++ {
+            var property = properties[Int(i)];
+            
+            // retrieve the property name by calling property_getName function
+            var cname = property_getName(property);
+            
+            // covert the c string into a Swift string
+            var name = String.fromCString(cname);
+            results.append(name!);
+        }
+        
+        // release objc_property_t structs
+        free(properties);
+        
+        return results;
+    }
+    
+    func classNameAsString() -> String {
+        
+        let thisClass: AnyClass = self.classForCoder
+        let classString = NSStringFromClass(thisClass)
+        return classString
+    }
+    
+}
