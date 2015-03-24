79043dff3db0d57352558b1a4fcb274e9f713e7c
mchoe <michael@straussmade.com>
2015-03-24 17:12:42 -0500

    Initial Commit

---
+++ Breakfast/Breakfast.h
@@ -0,0 +1,19 @@
+//
+//  Breakfast.h
+//  Breakfast
+//
+//  Created by collardgreens on 3/24/15.
+//  Copyright (c) 2015 Strauss LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Breakfast.
+FOUNDATION_EXPORT double BreakfastVersionNumber;
+
+//! Project version string for Breakfast.
+FOUNDATION_EXPORT const unsigned char BreakfastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Breakfast/PublicHeader.h>
+
+
